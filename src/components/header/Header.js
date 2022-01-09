c9576b2642851f83e8fb66aaa75d1576763c9b44
Nurhanfront <front@studionomad.kz>
2022-01-09 16:39:47 +0600

    feat: excel page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,11 @@ import { ExcelComponent } from "@core/ExcelComponent";
 
 export class Header extends ExcelComponent {
     static className = 'excel__header'
+    constructor($root) {
+        super($root, {
+            name: 'Header',
+        })
+    }
 
     toHTML() {
         return `
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,17 +1,41 @@
+import { capitalize } from "@core/utils"
+
 export class DomListener {
-    constructor($root) {
+    constructor($root, listeners = []) {
         if (!$root) {
             throw new Error(`no $root provider for DomListener`)
         }
         this.$root = $root
+        this.listeners = listeners
     }
 
     initDOMListeners() {
+        this.listeners.forEach(listener => {
+            const method = getMethodName(listener)
 
+            if (!this[method]) {
+                const name = this.name || ''
+                throw new Error(`Method ${method} is not implemented in ${name} Component`)
+            }
+            // Тоже самое что и addEventListener
+            this[method] = this[method].bind(this)
+            this.$root.on(listener, this[method])
+        })
     }
 
     removeDOMListeners() {
+        this.listeners.forEach(listener => {
+            const method = getMethodName(listener)
+            console.log('removeDomElent', method);
+            this.$root.off(listener, this[method])
+        })
 
     }
 
+}
+
+// input => onInput
+
+function getMethodName(eventName) {
+    return 'on' + capitalize(eventName)
 }
